75c41871ae06918960cf272bbd1c13cf099bf59b
tnb24 <tanaybhardwaj24@gmail.com>
2022-02-10 17:01:45 +0000

    d

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['avatars.githubusercontent.com','https://avatars.githubusercontent.com', 'http://avatars.githubusercontent.com'],
+  },
 }
 
 module.exports = nextConfig
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
 import type { NextPage } from 'next'
 import { useSession, signIn, signOut } from "next-auth/react"
-
+import Image from 'next/image'
 const Home: NextPage = () => {
   const { data: session } = useSession()
   if (session) {
     return (
       <>
         Signed in as {session.user.email} <br />
+        <Image src={session.user?.image} alt="profile picture of you" layout='fill'/>
         <button onClick={() => signOut()}>Sign out</button>
       </>
     )
